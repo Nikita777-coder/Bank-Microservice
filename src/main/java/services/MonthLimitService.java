636d16d574149c969d49a0a55d3b5f7bef490bb8
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-04-07 20:29:18 +0300

    add type of operation

--- src/main/java/services/MonthLimitService.java
+++ src/main/java/services/MonthLimitService.java
@@ -1,8 +1,11 @@
 package services;
 
+import entities.MonthLimitEntity;
+import enums.TransferType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import repositories.MonthLimitRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,12 +20,16 @@ public class MonthLimitService {
     @Value("${server-configs.month-limit.defaultLimitNumber}")
     private BigDecimal defaultLimitNumber;
     public Optional<MonthLimitEntity> findLastMonthLimitByClientIdAndMonth(UUID clientId, Month month) {
-        return monthLimitRepository.findLastMonthLimitByClientIdAndMonth(clientId, month);
+        return monthLimitRepository.findAllByClientIdAndTimestampMonthValueOrderByTimestampDesc(
+                clientId,
+                month.getValue()
+        ).stream().findFirst();
     }
     public MonthLimitEntity createDefaultMonthLimit(UUID clientId) {
         return MonthLimitEntity
                     .builder()
                     .clientId(clientId)
+                    .monthLimitType(TransferType.SERVICES)
                     .timestamp(LocalDateTime.now())
                     .limitNumber(defaultLimitNumber)
                     .limitBalance(defaultLimitNumber)
