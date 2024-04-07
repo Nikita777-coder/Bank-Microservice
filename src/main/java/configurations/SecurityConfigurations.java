f45c10ff3e2168f49cd2b886bfeb155626bcd4c1
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-04-07 20:36:30 +0300

    add SecurityConfigurations

---
+++ src/main/java/configurations/SecurityConfigurations.java
@@ -0,0 +1,25 @@
+package configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/**").authenticated()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
+
